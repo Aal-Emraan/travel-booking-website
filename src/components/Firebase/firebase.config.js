eb006329954bf8cd512aba9e75828784607c347f
Aal Emraan <developer.aalemraan@gmail.com>
2021-10-28 23:47:32 +0600

    firebase connected|

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+    apiKey:process.env.API_KEY,
+    authDomain:process.env.AUTH_DOMAIN,
+    projectId:process.env.PROJECT_ID,
+    storageBucket:process.env.STORAGE_BUCKET,
+    messagingSenderId:process.env.MESSAGING_SENDER_ID,
+    appId:process.env.APP_ID
+    // apiKey: "AIzaSyBb_cqPfoMMTVC1fW6O-uP1XkJfKREEFFs",
+    // authDomain: "travel-guru-d393c.firebaseapp.com",
+    // projectId: "travel-guru-d393c",
+    // storageBucket: "travel-guru-d393c.appspot.com",
+    // messagingSenderId: "896075123264",
+    // appId: "1:896075123264:web:c7ab3af8974eb1ca89eda5"
+  };
+
+export default firebaseConfig;
